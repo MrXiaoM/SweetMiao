b970e0d387cd275c5287b723778246233c2edb53
人間工作 <coolxiaom95@gmail.com>
2024-12-03 16:27:12 +0800

    remove sample command

--- src/main/java/top/mrxiaom/miao/commands/CommandMain.java
+++ src/main/java/top/mrxiaom/miao/commands/CommandMain.java
@@ -23,9 +23,6 @@ public class CommandMain extends AbstractModule implements CommandExecutor, TabC
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length == 1 && "hello".equalsIgnoreCase(args[0])) {
-            return t(sender, "Hello World!");
-        }
         if (args.length == 1 && "reload".equalsIgnoreCase(args[0]) && sender.isOp()) {
             plugin.reloadConfig();
             return t(sender, "&a配置文件已重载");
@@ -34,15 +31,12 @@ public class CommandMain extends AbstractModule implements CommandExecutor, TabC
     }
 
     private static final List<String> emptyList = Lists.newArrayList();
-    private static final List<String> listArg0 = Lists.newArrayList(
-            "hello");
-    private static final List<String> listOpArg0 = Lists.newArrayList(
-            "hello", "reload");
+    private static final List<String> listOpArg0 = Lists.newArrayList("reload");
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         if (args.length == 1) {
-            return startsWith(sender.isOp() ? listOpArg0 : listArg0, args[0]);
+            return startsWith(sender.isOp() ? listOpArg0 : emptyList, args[0]);
         }
         return emptyList;
     }
